83db31dcda3b132177a02300865bf051b6f16279
Nitish Bhardwaj <nitishmantu7@gmail.com>
2021-09-28 16:46:21 +0530

    navlink added

--- src/component/NavbarRight.js
+++ src/component/NavbarRight.js
@@ -1,32 +1,30 @@
-import React, {useState} from 'react';
+import React from 'react';
 import NavbarRightHeader from './NavbarRightComponent/NavbarRightHeader';
 //import Icon from './navbarleftComponent/Icon';
 import NavbarRightFooter from './NavbarRightComponent/NavbarRightFooter';
 //import MainContainer from './MainContainer';
 //import BegineerCourse from './courseStructure/BegineerCourse';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
   
 
 export default function NavbarRight() {
 
-    const [isActive , setActive] = useState(false);
-
-    const handleToggle = () => {
-        setActive(!isActive);
-    }
+  
 
 
 
     return (
-        <div className={isActive ? 'right-sidebar active' : 'right-sidebar'} onClick = {handleToggle}>
+        <div className="right-sidebar">
             <NavbarRightHeader iconName=" ri-menu-3-fill icon"
             fontSize='30' color="#8c8c8e" title='Facebook' />
 
-<ul>
-<Link to="/">Home</Link>
-    
-</ul>
+            <ul className="nav">
+               
+                <li className="nav-link"><NavLink to="/BegineerCourse" activeClassName="active">Begineer front-end Courses</NavLink></li>
+                <li className="nav-link"><NavLink to="/IntermediateCourse" activeClassName="active">Intermediate front-end Courses</NavLink></li>
+                <li className="nav-link"><NavLink to="/ComingSoon" activeClassName="active">Senior front-end Courses</NavLink></li>
+            </ul>
 
 
 
