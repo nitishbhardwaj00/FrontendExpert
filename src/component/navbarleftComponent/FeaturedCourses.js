c5c88e718ef08e39a14770b404ebb745f64f2bcd
Nitish Bhardwaj <nitishmantu7@gmail.com>
2021-09-30 16:10:36 +0530

    link-fixed

--- src/component/navbarleftComponent/FeaturedCourses.js
+++ src/component/navbarleftComponent/FeaturedCourses.js
@@ -7,11 +7,11 @@ return(
     <section className="featuredcourses">
         
             <div className="course-section pt-30 pb-15">
-                <Courses title= "HTML" icon = "ri-html5-fill" path = "BegineerCourse"/>
-                <Courses title= "Css" icon = "ri-css3-fill" path = "BegineerCourse"/>
-                <Courses title= "Scss" icon = " " iconText="scss" path = "BegineerCourse"/>
-                <Courses title= "javaScript" icon = " " iconText="JS" path = "IntermediateCourse"/>
-                <Courses title= "jQuery" icon = " ri-code-box-fill" iconText="" path = "IntermediateCourse"/>
+                <Courses title= "HTML" icon = "ri-html5-fill" path = "/BegineerCourse"/>
+                <Courses title= "Css" icon = "ri-css3-fill" path = "/BegineerCourse"/>
+                <Courses title= "Scss" icon = " " iconText="scss" path = "/BegineerCourse"/>
+                <Courses title= "javaScript" icon = " " iconText="JS" path = "/IntermediateCourse"/>
+                <Courses title= "jQuery" icon = " ri-code-box-fill" iconText="" path = "/IntermediateCourse"/>
                 <Courses title= "React.js" icon = " ri-reactjs-line" iconText="" path = "/ComingSoon"/>
             </div>
 
