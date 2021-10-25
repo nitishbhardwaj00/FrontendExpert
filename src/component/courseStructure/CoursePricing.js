e3647fa67bba356afab22f264ec279ed06b7d5c2
Nitish Bhardwaj <nitishmantu7@gmail.com>
2021-10-25 16:00:45 +0530

    Update CoursePricing.js

--- src/component/courseStructure/CoursePricing.js
+++ src/component/courseStructure/CoursePricing.js
@@ -10,7 +10,7 @@ export default function CoursePricing(props){
                 <h1 className="lightheadline">Your Program</h1>
                 <p className="lightbodytext">{props.progText}</p>
                 <h2 className="lightheadline mt-15 mb-10">How to pay?</h2>
-                <p className="lightbodytext"> You just have to scan the QR through any payment getway app like <b>(phone pe, google pay & Paytm )</b>. At the time of Payment you have to write your valid email address and we will send the zoom link with timing. for any query please  <a href="mailTo:mnitishbhardwaj@gmail.com">send us an email</a> or call <a href="to:8882137405">8882137405</a>.</p>
+                <p className="lightbodytext"> You just have to scan the QR through any payment getway app like <b>(phone pe, google pay & Paytm )</b>. At the time of Payment you have to write your valid email address and we will send the zoom link with timing. for any query please  <a href="mailTo:mnitishbhardwaj@gmail.com">send us an email</a> or call <a href="tel:8882137405">8882137405</a>.</p>
                 <Link to= "/Form" className="primary-btn btn mt-30">Connect with us</Link>
             </div>
             <div className="pricing-container">
